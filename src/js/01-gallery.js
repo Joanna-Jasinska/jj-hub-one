839cb551f791460598a618cccd0a18b15bfa8b85
Joanna <jjasinska.coding@gmail.com>
2023-03-09 03:06:12 +0100

    changed class names

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,44 +2,57 @@ import { galleryItems } from './gallery-items';
 import { dices } from './dices';
 import { f } from './functions';
 import { newGame } from './dice-newgame';
-// import SimpleLightbox from 'simplelightbox';
-// import 'simplelightbox/dist/simple-lightbox.min.css';
 
 console.log(dices);
 console.log(newGame);
 console.log(f.getRandom());
 
-const galleryDiv = document.querySelector('.gallery');
+const diceBag = document.querySelector('.diceBag');
+// console.log(diceBag);
+
+function refreshDice(o) {
+  if (o.classList.contains('inactive')) {
+    o.classList.remove('inactive');
+  }
+}
+function rollDice(o) {
+  const diceFace = o.querySelector('.dice__rolled');
+  const diceMax = o.querySelector('.dice__max');
+  const dmax = parseInt(diceMax.innerText);
+  let dface = f.getRandom(dmax);
+  diceFace.innerText = `${dface}`;
+  o.style.backgroundColor = `${f.getDiceBackgroundColor(dface, dmax)}`;
+  diceMax.innerText = `${dmax}`;
+}
 
 for (const gi of newGame) {
-  galleryDiv.append(document.createElement('div'));
-  const div =
-    document.querySelectorAll('.gallery>div')[
-      document.querySelectorAll('.gallery>div').length - 1
+  diceBag.append(document.createElement('div'));
+  const dice =
+    document.querySelectorAll('.diceBag > div')[
+      document.querySelectorAll('.diceBag > div').length - 1
     ];
-  div.classList.add('gallery__item', 'gallery__image');
-  div.append(document.createElement('div'));
-  const diceFace = div.querySelector('div');
+  dice.classList.add('dice');
+  dice.append(document.createElement('div'));
+  const diceFace = dice.querySelector('div');
   diceFace.classList.add('dice__rolled');
   let dmax = f.getRandomDiceMax();
   let dface = f.getRandom(dmax);
   diceFace.innerText = `${dface}`;
-  div.style.backgroundColor = `${f.getDiceBackgroundColor(dface, dmax)}`;
-  div.prepend(document.createElement('div'));
-  const diceMax = div.querySelector('div');
+  dice.style.backgroundColor = `${f.getDiceBackgroundColor(dface, dmax)}`;
+  dice.prepend(document.createElement('div'));
+  const diceMax = dice.querySelector('div');
   diceMax.classList.add('dice__max');
   diceMax.innerText = `${dmax}`;
-  div.onclick = e => {
-    e.preventDefault();
-    let dface = f.getRandom(dmax);
-    diceFace.innerText = `${dface}`;
-    div.style.backgroundColor = `${f.getDiceBackgroundColor(dface, dmax)}`;
-    diceMax.innerText = `${dmax}`;
+  dice.onclick = e => {
+    if (!dice.classList.contains('inactive')) {
+      e.preventDefault();
+      dice.classList.add('inactive');
+    }
   };
   // link.append(document.createElement('img'));
   // link.classList.add('gallery__item');
   // link.href = gi.original;
-  // const img = div.querySelector('img');
+  // const img = dice.querySelector('img');
   // img.classList.add('gallery__image');
   // img.src = gi.preview;
   // img.alt = gi.description;
@@ -49,16 +62,15 @@ for (const gi of newGame) {
   //   e.preventDefault();
   // };
 }
-// const options = {
-//   captions: true,
-//   captionsData: 'alt',
-//   captionSelector: 'img',
-//   captionType: 'attr',
-//   captionsData: 'alt',
-//   captionPosition: 'bottom',
-//   captionDelay: 250,
-// };
-// let gallery = new SimpleLightbox('.gallery a', options);
-// gallery.on('show.simplelightbox', function () {
-//   //on show nothing :P
-// });
+diceBag.prepend(document.createElement('div'));
+const endTurn = diceBag.querySelector('div');
+endTurn.classList.add('button-end-turn');
+endTurn.innerText = 'End\nTurn';
+endTurn.onclick = e => {
+  e.preventDefault();
+  const haveDices = document.querySelectorAll('.dice');
+  for (let i = 0; i < haveDices.length; i++) {
+    refreshDice(haveDices[i]);
+    rollDice(haveDices[i]);
+  }
+};
