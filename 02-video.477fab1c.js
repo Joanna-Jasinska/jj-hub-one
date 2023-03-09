b8edfda23c34ac4a48bcc8d8f043b63fe4d801d8
Joanna-Jasinska <111195576+Joanna-Jasinska@users.noreply.github.com>
2023-03-09 19:08:45 +0000

    Deploying to gh-pages from @ Joanna-Jasinska/jj-hub-one@1f12741e5a9c76d6835dbcf5d942baafda3b00a3 🚀

--- 02-video.477fab1c.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("iframe"),t=new Vimeo.Player(e);function r(){let e=0;try{e=JSON.parse(localStorage.getItem("videoplayer-current-time"))}catch(t){e=0}return e}t.setCurrentTime(r()),t.on("timeupdate",(function(){t.getCurrentTime().then((function(e){let t=e||r();localStorage.setItem("videoplayer-current-time",`${t}`)}))}));
-//# sourceMappingURL=02-video.477fab1c.js.map
